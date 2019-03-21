4b18b180cb70c72c5736722c99678b7cb31dd8d5
unknown <tomormsby@gmail.com>
2019-03-21 16:39:27 +0100

    working and trimmed and commented

--- nn_test.py
+++ nn_test.py
@@ -11,7 +11,7 @@ import gc
 from sklearn.model_selection import train_test_split
 from keras.layers import Input, Dense, Activation, Dropout
 from keras.layers.advanced_activations import ReLU
-from keras.activations import tanh
+from keras.activations import tanh, sigmoid
 from keras.models import Model
 from keras.optimizers import SGD
 import sys
@@ -27,12 +27,12 @@ nb_epoch = 30
 
 def paths_list_from_directory(directory):
 
-    subdirs = next(os.walk(directory))[1]  # takes the immediate children of parent directory
-    # print("Sub directories: ", end="")
-    # print(*subdirs, sep=", ")
+    subdirs = next(os.walk(directory))[1]  # returns list of immediate children of parent directory
+
     path_list = []
+    # for folders in the list of subdirectories, add all the files within it to the path list
     for folder in subdirs:
-        # loop over files and make a list of tuples with label and path then add to the path list
+        # loop over files and make a list of full paths then add to the path list
         file_names = [os.path.join(directory, folder, f) for f in os.listdir(os.path.join(directory, folder))]
         path_list += file_names
 
@@ -86,7 +86,7 @@ def DataGenerator(img_addrs, batch_size, num_classes):
     while 1:
         # Ensure randomisation per epoch using np.random.shuffle
         # addrs_labels = list(zip(img_addrs, img_labels))
-        np.random.shuffle(addrs_labels)
+        np.random.shuffle(img_addrs)
         # img_addrs, img_labels = zip(*addrs_labels)
 
         X = []
@@ -102,71 +102,68 @@ def DataGenerator(img_addrs, batch_size, num_classes):
             # [2] Create a one-hot encoding with np.eye and append the one-hot vector to Y.
             Y.append(np.eye(num_classes)[label])  # takes the label from the load_image function and makes 1-hot.
 
-
         # [3] Commpare the count and batch_size (hint: modulo operation) and if so:
         # we use j+1 as count var was previously equivalent to this
+        # when we hit a value of j+1 that is a multiple of batch size we run below block
             if ((j+1) % batch_size) == 0:
-                # print(*Y)
                 #   - Use yield to return X,Y as numpy arrays with types 'float32' and 'uint8' respectively
                 X = np.array(X, dtype=np.float32)
                 X = X.reshape(batch_size, 2304)
                 Y = np.array(Y, dtype=np.uint8)
 
                 yield X, Y
+
                 #   - delete X,Y
                 del X
                 del Y
+
                 #   - set X,Y to []
                 X = []
                 Y = []
+
                 # garbage collect
                 gc.collect()
 
 
 if __name__ == "__main__":
 
-    # paths = paths_list_from_directory('PetImages')
-    # with warnings.catch_warnings():
-    #    for p in paths:
-    #        try:
-    #            io.imread(p)
-    #        except:
-    #            sys.stderr.write('Invalid Image...Deleting...' + str(p) + "\n")
-    #            os.remove(p)
-
+    # pull paths from function and shuffle these before splitting to train and val
     paths = paths_list_from_directory('PetImages')
     np.random.shuffle(paths)
 
-    # Use train test split
+    # Use train test split to split to default 0.75 train and 0.25 test
     train, val = train_test_split(paths)
 
-    X_train = []
-    Y_train = []
+    # Below section was for data creation without the generator so is no longer needed.
+    # X_train = []
+    # Y_train = []
 
-    for p in train:
-        tpl = load_image(p)
-        X_train.append(tpl[0])
-        Y_train.append(np.eye(nb_classes)[tpl[1]])
+    # for p in train:
+    #    tpl = load_image(p)
+    #    X_train.append(tpl[0])
+    #    Y_train.append(np.eye(nb_classes)[tpl[1]])
+    #
+    # X_val = []
+    # Y_val = []
+    #
+    # for p in val:
+    #     tpl = load_image(p)
+    #     X_val.append(tpl[0])
+    #     Y_val.append(np.eye(nb_classes)[tpl[1]])
 
-    X_val = []
-    Y_val = []
+    # lt = len(X_train)
+    # lv = len(X_val)
 
-    for p in val:
-        tpl = load_image(p)
-        X_val.append(tpl[0])
-        Y_val.append(np.eye(nb_classes)[tpl[1]])
+    # X_train = np.asarray(X_train)
+    # Y_train = np.asarray(Y_train)
+    # X_val = np.asarray(X_val)
+    # Y_val = np.asarray(Y_val)
 
-    lt = len(X_train)
-    lv = len(X_val)
+    # X_train = X_train.reshape(lt, 2304)
+    # X_val = X_val.reshape(lv, 2304)
 
-    X_train = np.asarray(X_train)
-    Y_train = np.asarray(Y_train)
-    X_val = np.asarray(X_val)
-    Y_val = np.asarray(Y_val)
-
-    X_train = X_train.reshape(lt, 2304)
-    X_val = X_val.reshape(lv, 2304)
-
+    # Build the network structure. We tried using tanh and sigmoid activation (non-linear) on hidden layers...
+    # ...but this did not improve results
     inputs = Input(shape=(2304,))
     x = inputs
 
@@ -175,48 +172,51 @@ if __name__ == "__main__":
     while i < 5:
         x = Dense(254)(x)
         x = ReLU()(x)  # Non-linearily
-        # x = Activation(tanh)(x)
+        #x = Activation(sigmoid)(x)
         x = Dropout(0.5)(x)
         i = i + 1
 
     predictions = Dense(nb_classes, activation='softmax')(x)
     model = Model(input=inputs, output=predictions)
 
-    # sgd = SGD(lr=0.01, decay=0, momentum=0, nesterov=False)
+    # gradient decent parameters
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
     model.compile(optimizer=sgd,
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
+    # print a summary of the model
     model.summary()
 
-    #history = model.fit(X_train, Y_train,
-    #                    batch_size=batch_size, epochs=nb_epoch,
-    #                    verbose=1, validation_data=(X_val, Y_val))
-    history = model.fit_generator(DataGenerator(train, Y_train, batch_size, nb_classes), epochs=nb_epoch,
+    # fit the model with our created generator, validate on the generator with the validation data over 10 batches
+    history = model.fit_generator(DataGenerator(train, batch_size, nb_classes), epochs=nb_epoch,
                                   steps_per_epoch=64, verbose=1,
-                                  validation_data=(X_val, Y_val))
-    # score = model.evaluate(X_val, Y_val, verbose=0)
-    score = model.evaluate_generator(DataGenerator(val, Y_val, len(X_val), nb_classes), verbose=0, steps=1)
+                                  validation_data=DataGenerator(val, batch_size, nb_classes),
+                                  validation_steps=10)
+
+    # score by doing a full run over the validation set
+    score = model.evaluate_generator(DataGenerator(val, len(val), nb_classes), verbose=0, steps=1)
 
     print('Test score:', score[0])
     print('Test accuracy:', score[1])
 
-    # Play times! Add something to play with the model.
-    # np.random.shuffle(paths)
+    # Added a quick and dirty interface to take images from the validation set, display them and their predicted label
     idx = 0
-    np.random.shuffle(val)
     while 1:
         cv2.destroyAllWindows()
+        # read original image and use load_image to get the greyscale cropped and resized input image to the network
         image = cv2.imread(val[idx])
         image_gs = load_image(val[idx])
+
+        # process the image into expected network input
         x_pred = np.array(image_gs[0])
         x_pred = x_pred.reshape(1, 2304)
+
+        # predict from the model
         label = model.predict(x_pred)
 
-        print(label)
-
+        # set label to the most likely category (Good boi! = Dog, of course)
         if label[0][0] > 0.5:
             label = 'Cat'
         else:
@@ -224,9 +224,11 @@ if __name__ == "__main__":
 
         # Print the classification on the image
         cv2.putText(image, label, (5, 120), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 255), 2, cv2.LINE_AA)
+
+        # Display the image with label printed on it
         cv2.imshow("Image", image)
         keypress = cv2.waitKey(0)
-        # if key back, key forward, esc
+        # Set to scroll through the dataset using 'd' to move forward, 'a' to move back and esc to exit
         if keypress == 97 and idx > 0:
             idx -= 1
         elif keypress == 100 and idx < len(paths):
