4b801727f9f364915d6aec922a14cd060c4456e6
unknown <tomormsby@gmail.com>
2019-03-21 16:10:33 +0100

    working network and generator

--- nn_test.py
+++ nn_test.py
@@ -14,19 +14,22 @@ from keras.layers.advanced_activations import ReLU
 from keras.activations import tanh
 from keras.models import Model
 from keras.optimizers import SGD
+import sys
+from skimage import io
+import warnings
 
 np.random.seed(1337)  # for reproducibility
 
 batch_size = 64
 nb_classes = 2
-nb_epoch = 10
+nb_epoch = 30
 
 
 def paths_list_from_directory(directory):
 
     subdirs = next(os.walk(directory))[1]  # takes the immediate children of parent directory
-    print("Sub directories: ", end="")
-    print(*subdirs, sep=", ")
+    # print("Sub directories: ", end="")
+    # print(*subdirs, sep=", ")
     path_list = []
     for folder in subdirs:
         # loop over files and make a list of tuples with label and path then add to the path list
@@ -39,61 +42,71 @@ def paths_list_from_directory(directory):
 def load_image(filename):
 
     # [1] Get the file category and make the conversion. If 'dog' assign it integer 1, if 'cat' assign it integer 0.
+    # check if string 'Dog' is in the path name (in the folder named Dog)
     if 'Dog' in filename:
         label = 1
     else:
         label = 0
 
     # [2] Load the image in greyscale with opencv.
+    # use cv2 image read function, and greyscale parameter
     image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
 
+    # [3] Find the dimension that is the smallest between the height and the width and assign it to the crop_dim var
+    # get values of height and width from image shape
     height, width = image.shape[:2]
-    crop_dim = None
 
-    # [3] Find the dimension that is the smallest between the height and the width and assign it to the crop_drim var
+    # create a tuple that flags 1 on the larger dimesion, height in position 0 and width in position 1
     if height < width:
         crop_dim = (0, 1)
     else:
         crop_dim = (1, 0)
 
     # [4] Crop the centre of the image based on the crop_dim dimension for both the height and width.
+    # use our crop_dim tuple in calculation to determine what the margin (distance from edge of image)
     margin = int(crop_dim[0] * (height-width)/2 + crop_dim[1] * (width-height)/2)
-    # print("margin size: " + str(margin))
-    image = image[(crop_dim[1]*margin):(width - crop_dim[1]*margin),(crop_dim[0]*margin):(height-crop_dim[0]*margin)]
+    # use the crop_dim and calculated margin to exclude the margin from the appropriate dimension
+    image = image[(crop_dim[1]*margin):(width - crop_dim[1]*margin), (crop_dim[0]*margin):(height-crop_dim[0]*margin)]
 
     # [5] Resize the image to 48 x 48 and divide it with 255.0 to normalise it to floating point format.
-    image = cv2.resize(image,(48,48))
+    # use opencv resize function
+    image = cv2.resize(image,(48, 48))
+
+    # set the image as a float type and divide by 255.0 to normalize
+    image.astype(float)
+    image = image/255.0
 
     return image, label
 
 
 # noinspection PyPep8Naming,PyUnusedLocal
-def DataGenerator(img_addrs, img_labels, batch_size, num_classes):
+def DataGenerator(img_addrs, batch_size, num_classes):
 
+    # We removed the img_labels from DataGenerator as the load_image returns a label from the path anyway
     while 1:
-        # Ensure randomisation per epoch
-        addrs_labels = list(zip(img_addrs, img_labels))
+        # Ensure randomisation per epoch using np.random.shuffle
+        # addrs_labels = list(zip(img_addrs, img_labels))
         np.random.shuffle(addrs_labels)
-        img_addrs, img_labels = zip(*addrs_labels)
+        # img_addrs, img_labels = zip(*addrs_labels)
 
         X = []
         Y = []
 
-        # count = 0
-
+        # we removed count var from here, loop over range of images
         for j in range(len(img_addrs)):
 
             # [1] Call the load_images function and append the image in X.
-            image = load_image(img_addrs[j])
-            X.append(image[0])
+            image, label = load_image(img_addrs[j])
+            #### used to have resize
+            X.append(image)
             # [2] Create a one-hot encoding with np.eye and append the one-hot vector to Y.
-            # Y.append(np.eye(num_classes)[image[1]])
-            Y.append(img_labels[j])
-            # count += 1
+            Y.append(np.eye(num_classes)[label])  # takes the label from the load_image function and makes 1-hot.
+
 
         # [3] Commpare the count and batch_size (hint: modulo operation) and if so:
+        # we use j+1 as count var was previously equivalent to this
             if ((j+1) % batch_size) == 0:
-                print("I yield!")
+                # print(*Y)
                 #   - Use yield to return X,Y as numpy arrays with types 'float32' and 'uint8' respectively
                 X = np.array(X, dtype=np.float32)
                 X = X.reshape(batch_size, 2304)
@@ -111,17 +124,15 @@ def DataGenerator(img_addrs, img_labels, batch_size, num_classes):
 
 
 if __name__ == "__main__":
-    # paths = paths_list_from_directory('./PetImages')
 
-    # for p in paths:
-    #    try:
-    #        load_image(p)
-    #    except AttributeError:
-    #        print('Invalid Image...Deleting...')
-    #        os.remove(p)
-    #    except cv2.error:
-    #        print('Invalid Image in OpenCV...Deleting...')
-    #        os.remove(p)
+    # paths = paths_list_from_directory('PetImages')
+    # with warnings.catch_warnings():
+    #    for p in paths:
+    #        try:
+    #            io.imread(p)
+    #        except:
+    #            sys.stderr.write('Invalid Image...Deleting...' + str(p) + "\n")
+    #            os.remove(p)
 
     paths = paths_list_from_directory('PetImages')
     np.random.shuffle(paths)
@@ -143,7 +154,6 @@ if __name__ == "__main__":
     for p in val:
         tpl = load_image(p)
         X_val.append(tpl[0])
-        # CHECK THIS BIT
         Y_val.append(np.eye(nb_classes)[tpl[1]])
 
     lt = len(X_train)
@@ -162,16 +172,17 @@ if __name__ == "__main__":
 
     i = 0
 
-    while i < 3:
+    while i < 5:
         x = Dense(254)(x)
-        # x = tanh()(x)  # Non-linearily
-        x = Activation(tanh)(x)
+        x = ReLU()(x)  # Non-linearily
+        # x = Activation(tanh)(x)
         x = Dropout(0.5)(x)
         i = i + 1
 
     predictions = Dense(nb_classes, activation='softmax')(x)
     model = Model(input=inputs, output=predictions)
 
+    # sgd = SGD(lr=0.01, decay=0, momentum=0, nesterov=False)
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
     model.compile(optimizer=sgd,
@@ -184,7 +195,7 @@ if __name__ == "__main__":
     #                    batch_size=batch_size, epochs=nb_epoch,
     #                    verbose=1, validation_data=(X_val, Y_val))
     history = model.fit_generator(DataGenerator(train, Y_train, batch_size, nb_classes), epochs=nb_epoch,
-                                  steps_per_epoch=50, verbose=1,
+                                  steps_per_epoch=64, verbose=1,
                                   validation_data=(X_val, Y_val))
     # score = model.evaluate(X_val, Y_val, verbose=0)
     score = model.evaluate_generator(DataGenerator(val, Y_val, len(X_val), nb_classes), verbose=0, steps=1)
@@ -204,13 +215,15 @@ if __name__ == "__main__":
         x_pred = x_pred.reshape(1, 2304)
         label = model.predict(x_pred)
 
-        if label[0][0] == 1:
+        print(label)
+
+        if label[0][0] > 0.5:
             label = 'Cat'
         else:
-            label = 'Dog'
+            label = 'Good boi!'
 
         # Print the classification on the image
-        cv2.putText(image, label, (5, 150), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(image, label, (5, 120), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 255), 2, cv2.LINE_AA)
         cv2.imshow("Image", image)
         keypress = cv2.waitKey(0)
         # if key back, key forward, esc
